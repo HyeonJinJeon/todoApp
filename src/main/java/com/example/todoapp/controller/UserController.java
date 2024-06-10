f357f746c598538a02069c8dded067b78ff40bdf
h_jinnny <nd3807@naver.com>
2024-06-10 16:16:18 +0900

    Initial commit

---
+++ src/main/java/com/example/todoapp/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.todoapp.controller;
+
+import java.util.List;
+
+import com.example.todoapp.entity.User;
+import com.example.todoapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ src/main/java/com/example/todoapp/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.todoapp.service;
+
+import com.example.todoapp.repository.UserRepository;
+import com.example.todoapp.entity.User;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
